a6b13cb6b156b8cfd6cce4a18e36e282e1c80b42
cshamback <149506027+cshamback@users.noreply.github.com>
2024-12-27 18:27:20 +0000

    Added csv formatting and generation.

---
+++ PA-1-Source.py
@@ -0,0 +1,34 @@
+# Cody Shamback, Liem Chau, Steve Nguyen
+# Brute Force and Euclid's GCD Algorithms
+
+from csvGen import *
+
+# generate 1000 pairs of random integers
+data = [
+    ["John", "25", "New York"],
+    ["Emma", "30", "London"],
+    ["Michael", "35", "Tokyo"]
+]
+formatData(data)
+
+test("test", data)
+
+# --------------------------------------------------------
+# BRUTE FORCE ALGS =======================================
+# --------------------------------------------------------
+
+# generate results spreadsheet for v1 and v2
+
+# generate statistics spreadsheet for v1 and v2
+
+# --------------------------------------------------------
+# EUCLIDEAN ALGS =========================================
+# --------------------------------------------------------
+
+# generate results spreadsheet for v1 and v2
+
+# generate statistics spreadsheet for v1 and v2
+
+# --------------------------------------------------------
+# CONCLUSIONS.TXT ========================================
+# --------------------------------------------------------
---
+++ csvGen.py
@@ -0,0 +1,60 @@
+import csv
+
+# each algorithm requires two output CSV files 
+# one function for each, takes name of file (depends on algorithm and version used) and 2D array of data 
+# rows and columns of 2D array correspond to rows and cols of spreadsheet!
+
+def test(filename, data):
+    with open(filename + '.csv', 'w', newline = '') as file: 
+        writer = csv.writer(file)
+
+        # hardcoded header row 
+        writer.writerow([" Name", " Age", " City"])
+
+        # array of data passed as parameter 
+        writer.writerows(data[0:])
+
+def resultsCSV(filename, data):
+    with open(filename + '.csv', 'w', newline = '') as file: 
+        writer = csv.writer(file)
+
+        # hardcoded header row 
+        writer.writerow(["Number One", " Number Two", " Their GCD", " Time Spent (Milliseconds)"])
+
+        # array of data passed as parameter 
+        writer.writerows(data[0:])
+
+def statsCSV(filename, data):
+    with open(filename + '.csv', 'w', newline = '') as file: 
+        writer = csv.writer(file)
+
+        # hardcoded header row 
+        writer.writerow(["Statistics", " Milliseconds"])
+
+        # array of data passed as parameter 
+        writer.writerows(data[0:])
+
+# add whitespace to items in 2D array so that the data is neatly lined up in the resulting csv file 
+
+def formatData(data):
+
+    # this array is the same length as the number of columns in the 2D array.
+    # for each column, it holds the longest string in that column 
+    maxLengths = [0] * len(data[0])
+
+    # find the longest string in each column, store in maxLengths
+    for i in range(len(data[0])): # iterate through columns in the first row 
+        for j in range(len(data)): # for each of those columns, go all the way down through every row 
+
+            print("Current item:", data[j][i], " Length: ", len(data[j][i]), " Max length: ", maxLengths[i])
+
+            if(len(data[j][i]) > maxLengths[i]):
+                maxLengths[i] = len(data[j][i])
+
+    # for each item, append maxLengths[col] - data[row][col].length ' ' chars to that item 
+
+    for i in range(len(data[0])): # iterate through columns in the first row 
+        for j in range(len(data)): # for each of those columns, go all the way down through every row 
+
+            numSpaces = maxLengths[i] - len(data[j][i])
+            data[j][i] = " " + data[j][i] + " " * (numSpaces) # add an extra whitespace at the beginning for readability 
