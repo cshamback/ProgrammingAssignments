b176d1bada2582c36e74ab8da1e1d417b72a1582
cshamback <149506027+cshamback@users.noreply.github.com>
2025-01-09 20:43:23 +0000

    Implemented and tested BFS

--- PA_7-8/algs.py
+++ PA_7-8/algs.py
@@ -1,3 +1,5 @@
+from dataStructs import *
+
 # ------------------------------------------------------------------------------------------------------------------------------------------------------
 # BFS (PA-7) ===========================================================================================================================================
 # ------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -6,15 +8,58 @@
 #   Closest vertices visited first - "level by level"
 #   Begin with any node. Traverse all adjacent nodes, then all nodes adjacent to those 
 
-# data = graph to be searched
-# visited = array of booleans; represents which nodes have been visited already
-# target = integer (node ID) to look for 
-# currentNode = ID of node being searched (integer, not the actual node)
-# return value: path from starting point to target value 
+def bfs(s, e, data): # s = start node, e = end node, data = graph represented as 2d array 
+    # broken up BFS into two smaller methods for simplicity  
+    prev = solve(s, data)
+    return reconstructPath(s, e, prev)
 
-def bfs(data, visited, target, currentNode):
+def solve(s, data): # most of the work for BFS done here
+    q = Queue() # create a new, empty queue
+    q.enqueue(s) # add start node to queue 
 
-    pass
+    visited = [False for node in data] # one value of false (not visited) for each node in the graph
+    visited[s] = True # but we did visit the start node
+
+    prev = [None for node in data] # path from start to end is empty 
+
+    while(len(q.getList()) > 0): # while queue is not empty 
+        node = q.dequeue()
+        neighbors = data[node] # data[node] returns a list of neighbors that node has 
+
+        # node is the node we're currently visiting
+        # next is the neighbor of node we're currently looking at 
+        
+        for next in neighbors: 
+            if not visited[next]: # loop over all unvisited nodes 
+                q.enqueue(next) # find a next unvisited node 
+
+                visited[next] = True # mark node as visited 
+                prev[next] = node # add parent of next node to prev array - "to get to next, we came from node"
+
+                # result example: prev = [None, 0, 1, None, 2] => path looks like this: [start, 1, 2, 4]
+
+    return prev # return path we just built 
+
+def reconstructPath(s, e, prev): # ensures path is a valid path from S -> E and returns that path 
+    # reconstruct path backwards from e to s
+    path = []
+
+    # iterate through prev array from e to s
+    # stop if we find a null value 
+    curr = e 
+    while curr is not None: 
+        path.append(curr)
+        curr = prev[curr]
+
+    path.reverse()
+
+    # check if path actually starts at s, 
+    # therefore s and e are connected in the graph 
+    if len(path) > 0 and path[0] == s:
+        return path
+
+    # if s and e are not connected, return nothing 
+    return []
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------
 # DFS (PA-8) ===========================================================================================================================================
