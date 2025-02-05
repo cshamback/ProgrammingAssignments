f39be21715ba335dca38b7447f9c3ea25643330a
RiceCodes5 <steve.nguyen5245@gmail.com>
2025-02-04 22:43:16 -0600

    Revert "Formatting CSV"
    This reverts commit 7e7d4cfdf27cda04460c83423b1555dba28a5841.

--- PA_2/csvGen.py
+++ PA_2/csvGen.py
@@ -4,16 +4,13 @@ import math
 #All this does is generate a simple spreadsheet (Formatting is TBD)
 def csvGen(fileName, data):
     # Places the csv file in output with an appropriate name.
-    with open("PA_2/output/" + fileName + '.csv', 'w', newline=' ') as file: 
+    with open("PA_2/output/" + fileName + '.csv', 'w') as file:
         # header row/titles 
         fieldnames = ["Input Size ", " Value of n *log(n) ", " Time Spent (Milliseconds) ", " Value of n * log(n) / Time "]
 
         writer = csv.writer(file) # create writer object 
         writer.writerow(fieldnames) # write a single row for the headers
-
-        # Ensures proper number formatting in rows
-        formatted_data = [[f"{x}", f"{y:.6f}", f"{z:.6f}", f"{w:.6e}"] for x, y, z, w in data]
-        writer.writerows(formatted_data) # write the data into more rows and keeps it formatted
+        writer.writerows(data[0:]) # write the data into more rows 
 
 # ------------------------------------------------------------------------
 # STATS ==================================================================
@@ -31,12 +28,7 @@ def nLogn(n):
 def getStats(arr, timeSpent):
 
     timeComplexity = nLogn(len(arr))
-
-    # Just makes sure we don't divide by zero
-    if timeSpent == 0:
-        timeComplexityScaled = 0
-    else:
-        timeComplexityScaled = float((timeComplexity) / timeSpent)
+    timeComplexityScaled = float((timeComplexity) / timeSpent)
 
     # return the row. all are floats/ints except for timeComplexityScaled, which is a string in scientific notation
     result = [len(arr), timeComplexity, timeSpent, format(timeComplexityScaled, 'e')]
