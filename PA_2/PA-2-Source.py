13beaa362b810a18513a75fcba488881f687c19b
Rykovs-Right-Arm <lc2128@jagmail.southalabama.edu>
2025-02-04 21:57:09 -0600

    reports

--- PA_2/PA-2-Source.py
+++ PA_2/PA-2-Source.py
@@ -1,6 +1,6 @@
 from csvGen import * 
 from mergeSort import * 
-
+import time
 # ------------------------------------------------------------------------
 # PREMADE ARRAYS =========================================================
 # ------------------------------------------------------------------------
@@ -10,18 +10,31 @@ from mergeSort import *
 bigArray = []
 
 #Because arrays start at index 0, we have to make it repeat this 8 times to get our 9 arrays.
-for instance in range (9):
+for instance in range (10):
     #Create a sub array so that we can append to the big array to get our 9 different arrays.
     subArray = []
 
     #Fills the array with up to 20 elements. It can be any number, I just did 20 so my computer won't die.
-    for i in range(instance * 1000):
+    for i in range(instance * 1):
         #The values of said elements are between 1 and 99
         subArray.append(randomNumberFill(1,99))
     bigArray.append(subArray) 
-
 # ------------------------------------------------------------------------
-# UI SECTION =====================================================================
+# SORT SECTION =============================================================
+# ------------------------------------------------------------------------
+# Iterates through every element in the bigArray array
+for list in bigArray:
+    #starts recording the time
+    start = time.time()
+    # Sorting the current element using merge sort
+    mergeSort(0,len(list),list)
+    # Stops recording the time and subtracts from the start to get the elapsed time
+    end = time.time()
+    elapsed = end - start
+    # prints out the elapsed time
+    print("%.10f" % elapsed)
+# ------------------------------------------------------------------------
+# UI SECTION =============================================================
 # ------------------------------------------------------------------------
 
 # Step 1 - tell user they can input a number 1 ... 9 and press any other key to escape   
@@ -29,22 +42,24 @@ for instance in range (9):
 #this while operation allows multiple instances of this without needing to restart the program
 while True:
     try:
-        x = int(input("Select a number from 1 to 9 inclusive. Or you can type 'EXIT' to leave. "))
-        x = x - 1 # allow for 0 indexing while also letting user pick 1...9
+        # User selects an array to see
+        SelectedArray = int(input("Select a number from 1 to 9 inclusive. Or you can type 'EXIT' to leave. "))
     
-        if(x >= 0 and x < 9): # x can be 0-8, for indexes in bigArray
-            print(f"There are {len(bigArray[x])} elements in Array_{x + 1}")
+        if(SelectedArray > 0 and SelectedArray <= 9): # x can be 0-8, for indexes in bigArray
+            print(f"There are {len(bigArray[SelectedArray])} elements in Array_{SelectedArray}")
  
             #Displays the array before it was sorted
-            print(f"Displaying Array_{x + 1}")
-            print(bigArray[x])
+            print(f"Displaying Array_{SelectedArray}")
+            print(bigArray[SelectedArray])
 
             #Displays the array after it was sorted
-            print(f"Displaying sorted Array_{x + 1}")
-            print(mergeSort(0,len(bigArray[x]),bigArray[x]))
+            print(f"Displaying sorted Array_{SelectedArray}")
+            print(mergeSort(0,len(bigArray[SelectedArray])),bigArray[SelectedArray])
         else:
+            # Just in case the user's input is out of the range
             print("Try again")     
     except ValueError:
+        # In case the user types anything that isn't an integer 
         print("Exiting...")
         break
 
