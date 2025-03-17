fbdbca1f369b8f5bab7b41c11e5054ee02216cb6
cshamback <ets2122@jagmail.southalabama.edu>
2025-03-17 18:50:24 -0500

    Made objects for Tasks for easier data manipulation. STILL NEED TO ADD A SORT FOR TASK END TIME.

--- PA-5/main.py
+++ PA-5/main.py
@@ -7,16 +7,30 @@ i = 0
 # index corresponds to task number 
 # ie. task #3 is the 3rd one the user inputs values for, and it's in index 2 of both arrays.
 earnings = []
-durations = []
+starts = []
+ends = []
 
 while(i < numTasks):
     currEarning = int(input("Enter the amount earned by task " + str(i + 1) + ": "))
-    currTime = int(input("Enter the duration of the task: "))
+    currStart = int(input("Enter the start time of the task: "))
+    currEnd = int(input("Enter the end time of the task: "))
 
     earnings.append(currEarning)
-    durations.append(currTime)
-    
+    starts.append(currStart)
+    ends.append(currEnd)
+
     i = i + 1
 
+print("+--------------------------------------------------+")
+print("| Task Number | Start Times | End Times | Earnings |")
+print("+--------------------------------------------------+") 
+
 for i in range(numTasks):
-    print("Task " + str(i) + " duration: " + str(durations[i]) + " earnings: " + str(earnings[i]))
+    numSpaces1 = len("| Task Number ") - len(str(i))
+    numSpaces2 = len("| Start Times ") - len(str(starts[i]))
+    numSpaces3 = len("| End Times ") - len(str(ends[i]))
+    numSpaces4 = len("| Earnings ") - len(str(earnings[i])) - 2
+
+    print("| " + str(i) + (" " * numSpaces1) + str(starts[i]) + (" " * numSpaces2) + str(ends[i]) + (" " * numSpaces3) + str(earnings[i]) + (" " * numSpaces4) + "|")
+
+print("+--------------------------------------------------+") 
