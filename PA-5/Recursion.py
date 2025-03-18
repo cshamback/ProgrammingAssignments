6504be33c4f9da12cf539edd51b6d7cae52b460d
Rykovs-Right-Arm <lc2128@jagmail.southalabama.edu>
2025-03-17 19:00:34 -0500

    RecursiveDP to-do list

---
+++ PA-5/Recursion.py
@@ -0,0 +1,14 @@
+import time
+import math
+from functools import cache
+
+times = []
+def RecursiveDP(time):
+    # TO-DO
+    # Have all the user's times stored in the array
+    # Create a memo and have program check if already in there
+    # If not, then just add it to the memo
+    # Recursively do this until end of array
+    # Sort the times using merge sort (thank you cody)
+    # Add all earnings and print into the skeleton
+    pass
