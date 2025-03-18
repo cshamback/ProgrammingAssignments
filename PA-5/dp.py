e633f6b342cf80495bbdd70ad135fcdd0004362f
cshamback <ets2122@jagmail.southalabama.edu>
2025-03-18 18:05:05 -0500

    All work for today (3/18) (i give up going to try again tomorrow)

--- PA-5/dp.py
+++ PA-5/dp.py
@@ -6,6 +6,7 @@ def addToMemo(task, memo):
     temp.append(task)
     memo.append(temp)
 
+# prints 2d array of tasks
 def printTasks(memo): 
     for row in range(len(memo)): 
         for col in range(len(memo[row])):
@@ -15,26 +16,16 @@ def printTasks(memo):
                 print(memo[row][col].name , end=" -> ") # print all on the same line, followed by an arrow symbol
         print()
 
+# prints list of task names 
+def printSingleList(arr):
+    for i in range(len(arr)):
+        if(i == len(arr) - 1):
+            print(arr[i].name)
+        else:
+            print(arr[i].name, end=" -> ")
+
 def dp(tasks):
-    # INITIALIZATION ------------------------------------------------------------
-    memo = [] # 2d array that cont ains all lists of tasks - array of subtasks
-
-    # merge sort all tasks by end time
-    temp = []
-    temp = mergeSort(0, len(tasks), tasks)
-    tasks = temp
-
-    # ADD ALL STARTING POINTS TO MEMO --------------------------------------------
-    # cannot start at any task, must be the first one or one that overlaps with it 
-    addToMemo(tasks[0], memo)
-
-    for i in range(1, len(tasks)):
-        if((tasks[i].start < tasks[0].start) or (tasks[i].end < tasks[0].end)):
-            addToMemo(tasks[i], memo)
-
-    printTasks(memo)
-
-    # FINISHED: RETURN MEMO SO IT CAN BE USED/PRINTED --------------------------
+    memo = [[]]
     return memo
 
 tasks = [Task(30, 0, 3, "Task 1"), Task(30, -1, 4, "Task 2"), Task(10, 1, 2, "Task 3"), Task(1, 4, 6, "Task 4"), Task(5, 7, 10, "Task 5"), Task(6, 9, 11, "Task 6")]
