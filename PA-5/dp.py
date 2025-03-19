130744a7f31bba977f3292e2b07c9de7e4c2108f
cshamback <ets2122@jagmail.southalabama.edu>
2025-03-19 09:38:42 -0500

    Restarting with dict instead of 2d array; created new helper method printTasks to print all lists of tasks in dict separated by arrows.

--- PA-5/dp.py
+++ PA-5/dp.py
@@ -1,33 +1,23 @@
 from task import *
 from merge import *
 
-def addToMemo(task, memo):
-    temp = []
-    temp.append(task)
-    memo.append(temp)
-
-# prints 2d array of tasks
-def printTasks(memo): 
-    for row in range(len(memo)): 
-        for col in range(len(memo[row])):
-            if(col == len(memo[row]) - 1):
-                print(memo[row][col].name) # print the last task in the arr without an arrow
-            else:
-                print(memo[row][col].name , end=" -> ") # print all on the same line, followed by an arrow symbol
-        print()
-
-# prints list of task names 
-def printSingleList(arr):
-    for i in range(len(arr)):
-        if(i == len(arr) - 1):
-            print(arr[i].name)
-        else:
-            print(arr[i].name, end=" -> ")
+# prints dictionary of tasks as lists separated by arrows
+def printTasks(memo):
+    for key in memo: 
+        print("Profit = " + str(key), end=": ")
+        listAsStr = " -> ".join(memo[key])
+        print(listAsStr)
+    print()
 
 def dp(tasks):
-    memo = [[]]
-    return memo
+    memo = {} # key = profit, value = list of tasks leading to profit 
+
+    # store list of tasks in memo where key = profit and value = [task]
+    for task in tasks:
+        memo[task.pay] = [task.name]
+    printTasks(memo)
+
+    # return value of max key in memo
 
 tasks = [Task(30, 0, 3, "Task 1"), Task(30, -1, 4, "Task 2"), Task(10, 1, 2, "Task 3"), Task(1, 4, 6, "Task 4"), Task(5, 7, 10, "Task 5"), Task(6, 9, 11, "Task 6")]
-results = dp(tasks)
-printTasks(results)
+dp(tasks)
